816cb3bd940bb534f638615eeb8a9265e4b2033c
prncenium <princecsk666@gmail.com>
2025-08-25 11:01:47 +0530

    github issue

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store} >
-    <BrowserRouter basename='/SmartHome-/'>
+    <BrowserRouter >
     <App />
     </BrowserRouter>
     
